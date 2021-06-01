e403a88dfc8024594506afc08f08585550d0c76d
Alan Enrique Maldonado Navarro <business.alanm@gmail.com>
2021-05-31 22:22:54 -0500

    Updated and finished filters

--- infrastructure/api/covid/read.go
+++ infrastructure/api/covid/read.go
@@ -17,8 +17,15 @@ type ByState struct {
 }
 
 type ByPrivPub struct {
-	Priv *map[string]*ByState
-	Pub  *map[string]*ByState
+	Priv      *map[string]*ByState
+	PrivCount int64
+	Pub       *map[string]*ByState
+	PubCount  int64
+}
+
+type ByAge struct {
+	AgesByState map[int]map[string]*ByState
+	AgesByCount map[int]int
 }
 
 func QueryCustom(c *fiber.Ctx) error {
@@ -75,6 +82,14 @@ func QueryCustom(c *fiber.Ctx) error {
 				Data:  filteredDocs,
 				Count: 1,
 			})
+		case "BYAGE":
+			filteredDocs := ConvertDataByAge(documents)
+			return c.Status(http.StatusOK).JSON(&models.Response{
+				Code:  http.StatusOK,
+				Error: nil,
+				Data:  filteredDocs,
+				Count: 1,
+			})
 		}
 	}
 
@@ -109,7 +124,9 @@ func ConvertDataByState(docs *[]*models.Case) *map[string]*ByState {
 
 func ConvertDataByPubPriv(docs *[]*models.Case) *ByPrivPub {
 	PubStates := map[string]*ByState{}
+	pubcount := 0
 	PrivStates := map[string]*ByState{}
+	privcount := 0
 
 	for _, each := range *docs {
 		switch each.Origen {
@@ -126,6 +143,7 @@ func ConvertDataByPubPriv(docs *[]*models.Case) *ByPrivPub {
 				state.Count++
 				state.Data = append(state.Data, each)
 			}
+			privcount++
 			break
 		case "PUB":
 			if state, isOk := PubStates[each.Entidad_Residencia]; !isOk {
@@ -140,14 +158,61 @@ func ConvertDataByPubPriv(docs *[]*models.Case) *ByPrivPub {
 				state.Count++
 				state.Data = append(state.Data, each)
 			}
+			pubcount++
 			break
 		}
 
 	}
 
 	final := ByPrivPub{
-		Priv: &PrivStates,
-		Pub:  &PubStates,
+		Priv:      &PrivStates,
+		Pub:       &PubStates,
+		PrivCount: int64(privcount),
+		PubCount:  int64(pubcount),
+	}
+
+	return &final
+}
+
+func ConvertDataByAge(docs *[]*models.Case) *ByAge {
+	statesByAge := map[int]map[string]*ByState{}
+	ages := map[int]int{}
+
+	for _, each := range *docs {
+		if _, isOk := ages[int(each.Edad)]; !isOk {
+			ages[int(each.Edad)] = 1
+		} else {
+			ages[int(each.Edad)]++
+		}
+
+		if curAgeStates, isOk := statesByAge[int(each.Edad)]; !isOk {
+			statesByAge[int(each.Edad)] = map[string]*ByState{}
+			statesByAge[int(each.Edad)][each.Entidad_Residencia] = &ByState{
+				State: each.Entidad_Residencia,
+				Count: 1,
+				Data:  []*models.Case{each},
+			}
+
+		} else {
+			if state, isOk := curAgeStates[each.Entidad_Residencia]; !isOk {
+				curAgeStates[each.Entidad_Residencia] = &ByState{
+					State: each.Entidad_Residencia,
+					Count: 1,
+					Data: []*models.Case{
+						each,
+					},
+				}
+			} else {
+				state.Count++
+				state.Data = append(state.Data, each)
+			}
+		}
+
+	}
+
+	final := ByAge{
+		AgesByState: statesByAge,
+		AgesByCount: ages,
 	}
 
 	return &final
