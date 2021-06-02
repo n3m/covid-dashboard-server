1e3635c850fe3e38a34976aab659dbf161d7c1a7
GG-kun <0205838@up.edu.mx>
2021-06-01 20:12:39 -0500

    Feature: Infected Graphic Filter

--- public/js/infected.js
+++ public/js/infected.js
@@ -6,70 +6,25 @@ var margin = {
     bottom: 100,
 };
 
-var width = 600;
-var height = 400;
+var width = 600 - margin.left - margin.right,
+height = 400 - margin.top - margin.bottom;
 
 var svg = d3.select("#chart-area").append("svg")
     .attr("width", width + margin.right + margin.left)
 	.attr("height", height + margin.top + margin.bottom);
 
-var g = svg.append("g")
-.attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
-
-axios.post('/covid', {
-    "responseType":"BYSTATE",
-}).then((response)=> {
-    console.log(response.data.data);
-    var keys = Object.keys(response.data.data);
-    var data = [];
-    keys.forEach((key) => {
-        data.push(response.data.data[key]);
-    });
-
-    var deadStates = data.map((d)=>{
-        return d["State"];
-    });
-    var x = d3.scaleBand()
-	.domain(deadStates)
-	.range([0,width])
-	.paddingInner(0.3)
-	.paddingOuter(0.3);
-
-    var maxDeadHeight = d3.max(data, (d) =>{
-        return d["Count"];
-    });
-    var y = d3.scaleLinear()
-	.domain([0,maxDeadHeight])
-	.range([height,0]);
-
-    var rectangles = g.selectAll("rect")
-    .data(data);
-
-    var bottomAxis = d3.axisBottom(x);
-    g.append("g")    
-        .attr("class", "bottom axis")    
-        .attr("transform", "translate(0, " + height + ")")    
-        .call(bottomAxis)        
-    .selectAll("text")
-        .attr("y", "10")
-        .attr("x", "10")
-        .attr("text-anchor", "end")
-    // X Label
-    g.append("text")
+var g = svg.append("g").attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+// X Label
+g.append("text")
     .attr("class", "x axis-label")
     .attr("x", width/2)
     .attr("y", height+40)
     .attr("font-size", "20px")
     .attr("text-anchor", "middle")
     .style("fill","black")
-    .text("State");
-
-    var leftAxis = d3.axisLeft(y).ticks(5)
-    g.append("g")
-        .attr("class", "left axis")
-        .call(leftAxis);
-    // Y Label
-    g.append("text")
+    .text("States");
+// Y Label
+g.append("text")
     .attr("class", "y axis-label")
     .attr("x", -(height/2))
     .attr("y", -60)
@@ -77,21 +32,112 @@ axios.post('/covid', {
     .attr("text-anchor", "middle")
     .style("fill","black")
     .text("Infected Count")
-	.attr("transform", "rotate(-90)");
+    .attr("transform", "rotate(-90)");
 
-    rectangles.enter()
-    .append("rect")
-    .attr("x", (d) => { 
-        return x(d["State"]); 
-    })
-    .attr("y", (d) => { 
-        return y(d["Count"]); 
-    } )
-    .attr("width", x.bandwidth())
-    .attr("height", (d) => { 
-        return height-y(d["Count"]); 
-    })
-    .attr("fill", "cyan");
-}).catch((error) => {
-    console.log(error);
-});
+var x = d3.scaleBand().range([0,width]).paddingInner(0.3).paddingOuter(0.3);
+var y = d3.scaleLinear().range([height,0]);
+
+var xAxisGroup = g.append("g")    
+    .attr("class", "x axis")    
+    .attr("transform", "translate(0, " + height + ")")
+var yAxisGroup = g.append("g")
+    .attr("class", "y axis");
+
+// Graphic starts with both sexs
+both();
+
+function both() {
+    setValues([],"black");
+}
+
+function male() {
+    setValues(
+        [
+            {
+                "eq":"HOMBRE",
+            },
+        ],
+        "cyan",
+    );
+}
+
+function female() {
+    setValues(
+        [
+            {
+                "eq":"MUJER",
+            },
+        ],
+        "pink",
+    );
+}
+
+function setValues(sex, color) {
+    axios.post('/covid', {
+        "responseType":"BYSTATE",
+        "sexo":sex,
+    }).then((response)=> {
+        update(response, color);
+    }).catch((error) => {
+        console.log(error);
+    });
+}
+
+var t = d3.transition().duration(250);
+
+var color = "cyan";
+
+function update(response) {
+    var keys = Object.keys(response.data.data);
+    var data = [];
+    keys.forEach((key) => {
+        data.push(response.data.data[key]);
+    });
+
+    var infectedStates = data.map((d)=>{
+        return d["State"];
+    });
+
+    var rectangles = g.selectAll("rect")
+        .data(data);
+
+    rectangles.exit()
+        .transition(t)
+            .attr("y", y(0))
+            .attr("height", 0)
+        .remove();
+
+    x.domain(infectedStates);
+
+    var maxInfectedHeight = d3.max(data, (d) =>{
+        return d["Count"];
+    });
+    y.domain([0,maxInfectedHeight]);
+
+    var bottomAxis = d3.axisBottom(x);
+    xAxisGroup.transition(t).call(bottomAxis);
+
+    var leftAxis = d3.axisLeft(y).ticks(5);
+    yAxisGroup.call(leftAxis);
+
+    rectangles.enter().append("rect")
+        .attr("y", y(0))    
+        .attr("height", 0)
+        .attr("x", (d) => { 
+            return x(d["State"]); 
+        })
+        .attr("width", x.bandwidth())
+        .merge(rectangles)
+        .transition(t)
+            .attr("x", (d) => { 
+                return x(d["State"]); 
+            })
+            .attr("width", x.bandwidth())
+            .attr("y", (d) => { 
+                return y(d["Count"]); 
+            } )
+            .attr("height", (d) => { 
+                return height-y(d["Count"]); 
+            })
+            .attr("fill", color);  
+}
